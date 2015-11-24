cb1a4a7bdca0402cd4e9acc8a80c5aeafe9f8bd0
catarinacustodio <catarinacustodio@users.noreply.github.com>
2015-11-24 10:11:13 +0000

    Update TAD_tabuleiro.py

--- TAD_tabuleiro.py
+++ TAD_tabuleiro.py
@@ -1,7 +1,7 @@
 
 def cria_tabuleiro(t):
 	if isinstance(t[0],(tuple)) and isinstance(t[1], (tuple)):
-		for i in (0, len(t[0])-1):                                             # uma vez que o tabuleiro e nxn, nr_linhas = nr_colunas
+		for i in (0, len(t[0])-1):                                             	   # uma vez que o tabuleiro e nxn, nr_linhas = nr_colunas
 			if isinstance(t[0][i], (tuple)) and isinstance(t[1][i], (tuple)):  # falta definir que dentro dos tuplos sejam num_inteiros
 				tabuleiro = t
 				return tabuleiro
