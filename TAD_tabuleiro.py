d021bc9f03213c2c5eaaa37fe245595ed271b0bf
Anfifo <afonseca-1995@hotmail.com>
2015-12-03 18:36:34 +0000

    added cria_tabuleiro

--- TAD_tabuleiro.py
+++ TAD_tabuleiro.py
@@ -10,6 +10,24 @@ def cria_tabuleiro(t):
 	else:
 		raise ValueError('cria_tabuleiro: argumentos invalidos')
 	
+# catarina,  essa que esta ai em cima é aquela antiga tua que tinha um erro neh? olha ve como achas da maneira
+# como esta definido aqui em baixo
+
+def cria_tabuleiro(tuplo):
+    #len do primeiro tuplo = numero de colunas
+    #len do segundo tuplo= numero de linhas 
+    #numero de linhas = numero de colunas 
+    tabuleiro_pt1=[]
+    for nr_de_linhas in range (len(tuplo[0])):
+        tabuleiro_pt1.append(tuple([0 for nr_colunas in range(len(tuplo[1]))])+(tuplo[1][nr_de_linhas],))
+    #juntar a base do tabueliro as especificacoes (possivelmente fazer uma funcao?)
+
+    tabuleiro = list(tuple(tuplo[0])+tuple(tabuleiro_pt1))
+    return tabuleiro 
+
+# para testar:  cria_tabuleiro((((2, ), (3, ), (2, ), (2, 2), (2, )), ((2, ), (1, 2), (2, ), (3, ), (3, ))))
+#--------------------------------------------
+
 
 def tabuleiro_dimensoes(t):
 	nr_linhas = len(t[0])
