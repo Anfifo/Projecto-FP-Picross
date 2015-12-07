fca96524df016cd5f90e293e4f2274acc9e16494
Anfifo <afonseca-1995@hotmail.com>
2015-12-07 15:51:05 +0000

    Update TAD_tabuleiro.py

--- TAD_tabuleiro.py
+++ TAD_tabuleiro.py
@@ -1,15 +1,4 @@
 
-def cria_tabuleiro(t):
-	if isinstance(t[0],(tuple)) and isinstance(t[1], (tuple)):
-		for i in (0, len(t[0])-1):                                             	   # uma vez que o tabuleiro e nxn, nr_linhas = nr_colunas
-			if isinstance(t[0][i], (tuple)) and isinstance(t[1][i], (tuple)):  # falta definir que dentro dos tuplos sejam num_inteiros
-				tabuleiro = t
-				return tabuleiro
-			else:
-				raise ValueError('cria_tabuleiro: argumentos invalidos')
-	else:
-		raise ValueError('cria_tabuleiro: argumentos invalidos')
-	
 # catarina,  essa que esta ai em cima é aquela antiga tua que tinha um erro neh? olha ve como achas da maneira
 # como esta definido aqui em baixo
 
