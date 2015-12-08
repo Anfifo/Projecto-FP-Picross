0ce0691171345adff3e40bce9cfacec5f848e181
Anfifo <afonseca-1995@hotmail.com>
2015-12-08 02:27:50 +0000

    Tudo do TAD tabuleiro funcional

--- Projecto.py
+++ Projecto.py
@@ -64,25 +64,24 @@ def coordenada_para_cadeia(coordenada):
 
 def cria_tabuleiro(tuplo):
     '''Recebe um tuplo com as especificacoes das linhas e colunas
-do tabuleiro e devolve o tabuleiro com estas especificacoes '''
+    do tabuleiro e devolve o tabuleiro com estas especificacoes '''
     if not (e_especificacao(tuplo)):
         raise ValueError('cria_tabuleiro: argumentos invalidos')
-    tabuleiro_pt1=[]
-    for nr_de_linhas in range (len(tuplo[0])):                  # para cada linha criamos uma lista de zeros
-        tabuleiro_pt1.append([0 for nr_colunas in range(len(tuplo[1]))]) #com o mesmo numero de colunas 
-    especificacoes=[tuplo,]                                              # que as especificacoes
-    return [tabuleiro_pt1,]+especificacoes
+    tab=[]
+    for nr_lin in range (len(tuplo[0])):                  # para cada linha criamos uma lista de zeros
+        tab.append([0 for nr_col in range(len(tuplo[1]))]) #com o mesmo numero de colunas
+    return [tab,]+[tuplo,]
 # representacao interna: e uma lista em que o primeiro elemento e a parte do tabuleiro onde entram
 # as jogadas e o segundo elemento e o tuplo das especificacoes   
 
 def tabuleiro_dimensoes(tabuleiro):
     '''recebe um elemento do tipo tabuleiro e retorna um tuplo
      com as dimensoes deste (linhas,colunas)'''
-    numero_linhas=len(tabuleiro[0])
-    numero_colunas=len(tabuleiro[0][0])
     if not(e_tabuleiro(tabuleiro)):                #funcao so valida se recebermos um tabueleiro
         raise ValueError('tabuleiro_dimensoes: argumentos invalidos')
-    return (numero_linhas, numero_colunas)
+    nr_lin=len(tabuleiro[0])
+    nr_col=len(tabuleiro[0][0])
+    return (nr_lin, nr_col)
 
 def tabuleiro_especificacoes(tabuleiro):
     ''' recebe um tabuleiro e retornar as especificacoes deste'''
@@ -94,52 +93,42 @@ def tabuleiro_celula(tabuleiro,coordenada):
     ''' recebe um tabuleiro e uma coordenada, retorna um elemento do tipo inteiro entre 0 e 2
     que corresponde ao valor contido na celula da coordenada: 0 se vazia, 1 se estiver branco
     e 2 se estiver preenchido'''
-    #if not(e_tabuleiro(tabuleiro) and e_coordenada(coordenada)):
-        #raise ValueError('tabuleiro_celula: argumentos invalidos')
-        #nao definida a e_coordenada no meu programa ainda 
-    return tabuleiro[coordenada[0]-1][coordenada[1]-1] # primeiro ve a linha e depois coluna
+    if not(e_tabuleiro(tabuleiro) and e_coordenada(coordenada)):
+        raise ValueError('tabuleiro_celula: argumentos invalidos') 
+    return tabuleiro[0][coordenada[0]-1][coordenada[1]-1] # primeiro ve a linha e depois coluna
 
 
 def tabuleiro_preenche_celula(tabuleiro,coordenada,inteiro):
     '''recebe um tabuleiro, uma coordenada e um inteiro entre 0 2, modifica o tabuleiro
-    e preenche a celula da coordenada com o inteiro, retornando o tabuleiro modificado'''
-    # ValueError com a mensagem ’tabuleiro_preenche_celula: argumentos invalidos’ caso algum
-    # dos argumentos introduzidos não seja válido.
-    
-    #if not(e_tabuleiro(tabuleiro) and e_coordenada(coordenada) and isinstance(inteiro,int)):
-        #raise ValueError('tabuleiro_preenche_celula: argumentos invalidos')
-        #nao definida a e_coordenada no meu programa ainda 
+    e preenche a celula da coordenada com o inteiro, retornando o tabuleiro modificado'''   
+    if not(e_tabuleiro(tabuleiro) and e_coordenada(coordenada) and isinstance(inteiro,int)):
+        raise ValueError('tabuleiro_preenche_celula: argumentos invalidos')
     tabuleiro[0][coordenada[0]-1][coordenada[1]-1]=inteiro
-    return tabuleiro # nao testada mas em principio deve funcionar
+    return tabuleiro
 
 
 def e_tabuleiro(universal):
     '''verificar se um universal e um tabuleiro ou nao, retornando true ou false'''
     try:
-        tabuleiro=universal[0]
+        tab=universal[0]
         especificacoes= universal[1]
-        nr_linhas= len(tabuleiro)
-
-        if not (len(universal)==2 and\
-            isinstance(tabuleiro,list) and\
-            isinstance(especificacoes, tuple)): # o tabuleiro e uma lista com listas
-            return False                        # as especificacoes e um tuplo
-
-            #verificar tabuleiro
-        for i in range(len(tabuleiro)):      
-            nr_colunas = len(tabuleiro[i]) #o nr de linhas tem de ser = ao nr colunas
-            if not(nr_colunas == nr_linhas and isinstance(tabuleiro[i], list)):
-                return False
-
-            for j in range(len(tabuleiro[i])):
-                elemento=tabuleiro[i][j]
-                if not ((isinstance(elemento,int) and 0<=elemento<=2)): #todos elementos do tabuleiro
-                    return False                                        #sao inteiros >=0 e <=2
+        nr_linhas= len(tab) 
         
-            # verificar especificacoes
+        for i in range(len(tab)):      
+            nr_colunas = len(tab[i]) #o nr de linhas tem de ser = ao nr colunas
+            for j in range(len(tab[i])):
+                if not(\
+                    0<= tab[i][j] <=2 and\
+                    len(universal) == 2 and\
+                    isinstance(tab,list) and\
+                    nr_colunas==nr_linhas and\
+                    isinstance(tab[i],list) and\
+                    isinstance(tab[i][j],int) and\
+                    isinstance(especificacoes,tuple)\
+                    ):
+                    return False
         if not(e_especificacao(especificacoes)):
             return False
-
     except(RuntimeError, TypeError, NameError,IndexError): # caso alguma das operacoes acima nao seja possivel
         return False                            # entao nao e um tabuleiro
     return True 
@@ -150,76 +139,82 @@ def e_tabuleiro(universal):
 def tabuleiros_iguais(tabuleiro_1,tabuleiro_2):
     if not (e_tabuleiro(tabuleiro_1) and e_tabuleiro(tabuleiro_2)):
         raise ValueError('cria_jogada: argumentos invalidos')
-    
-    tab1=tabuleiro_1[0]
-    espec_1=tabuleiro_especificacoes(tabuleiro_1)
-    tab2=tabuleiro_2[0]
-    espec_2=tabuleiro_especificacoes(tabuleiro_2)
+    try:
+        tab1=tabuleiro_1[0]
+        tab2=tabuleiro_2[0]
+        espec_1=tabuleiro_especificacoes(tabuleiro_1)
+        espec_2=tabuleiro_especificacoes(tabuleiro_2)
 
-    if not(tab1==tab2 and espec_1==espec_2):
-        return False
+        for lin in range(len(tab1)):
+            for col in range (len(tab1[lin])):
+                if not(\
+                    tab1[lin][col]==tab2[lin][col] and\
+                    espec_1[0][lin]==espec_2[0][lin]and\
+                    espec_1[1][col]==espec_2[1][col]\
+                    ):
+                    return False
+    except(IndexError):
+        return False  
     return True
 
 
 def escreve_tabuleiro(tabuleiro):
-
     if not(e_tabuleiro(tabuleiro)):                #funcao so valida se recebermos um tabueleiro
         raise ValueError('escreve_tabuleiro: argumentos invalidos')
     
-    tab=tabuleiro[0]
     especificacoes= tabuleiro_especificacoes(tabuleiro)
-    E_lin= especificacoes[0] #especificacoes das colunas
-    E_col = especificacoes[1] #especificacoes das linhas
-    nr_lin_col=len(tab) #numero de linhas e colunas
-    dim_Ecol=[len(E_col[x]) for x in range(nr_lin_col)] #len dos numeros
-    num_max_Ecol= max(dim_Ecol)
-    dim_Elin=[len(E_lin[x]) for x in range(nr_lin_col)]
-    num_max_Elin= max(dim_Elin)
+    nr_lin_col= tabuleiro_dimensoes(tabuleiro)[0]
+    E_lin= especificacoes[0] 
+    E_col= especificacoes[1]  
+    dim_Ecol= [len(E_col[x]) for x in range(nr_lin_col)]
+    dim_Elin= [len(E_lin[x]) for x in range(nr_lin_col)]
     valores=('?',".","x")
+    tab=tabuleiro[0]
 
-
-    for nr_espec in range(num_max_Ecol):          # print especificacoes das colunas
+    for i in range(max(dim_Ecol)):          # print especificacoes das colunas
         for colunas in range(nr_lin_col):
-            if dim_Ecol[colunas]-1>=(num_max_Ecol-1)-(nr_espec):
-                print(' ',E_col[colunas][dim_Ecol[colunas]-num_max_Ecol+(nr_espec)],end='   ')
+            if dim_Ecol[colunas]>=max(dim_Ecol)-(i):
+                print(' ',E_col[colunas][dim_Ecol[colunas]-max(dim_Ecol)+(i)],end='   ')
             else:
-                print('   ',end='   ')
-                
+                print('   ',end='   ')   
         print()
 
-    for linha in range (nr_lin_col): # print de cada elemento 
+    for l in range (nr_lin_col): # print de cada elemento 
         for coluna in range(nr_lin_col):
-            valor = tab[linha][coluna]
-            print ('[',valores[valor],']',end=' ')
-        for nr_espec in range(num_max_Elin):
-            try: 
-                if nr_espec==num_max_Elin-1:
-                    print(E_lin[linha][nr_espec],end='')    
+            print ('[',valores[tab[l][coluna]],']',end=' ')
+
+        for i in range(max(dim_Elin)):
+            if len(E_lin[l])>=i+1:
+                if i==max(dim_Elin)-1:
+                    print(E_lin[l][i],end='')
                 else:
-                    print(E_lin[linha][nr_espec],end=' ')
-            except(IndexError):
-                if nr_espec==num_max_Elin-1:
+                    print(E_lin[l][i],end=' ')    
+            else:
+                if i==max(dim_Elin)-1:
                     print('',end=' ')    
                 else:
                     print(' ',end=' ')
         print("|")
+    print()
 
 def tabuleiro_completo(tabuleiro):
     if not(e_tabuleiro(tabuleiro)):
         raise ValueError('tabuleiro_completo: argumentos invalidos')
-    tab=tabuleiro[0]
     nr_lin_col=tabuleiro_dimensoes(tabuleiro)[0]
-    espec=tabuleiro_especificacoes(tabuleiro)
-    E_lin=espec[0]
-    E_col=espec[1]
-    for lin_col in range(nr_lin_col):
-        linha=tab[lin_col]
-        if not (sum(E_lin[lin_col]) == linha.count(2) and\
-                len(E_lin[lin_col])-1 <= linha.count(1)):
-                    return False 
-        coluna=[tab[linhas][lin_col] for linhas in range(nr_lin_col)]
-        if not (sum(E_col[lin_col]) == coluna.count(2) and\
-                len(E_col[lin_col])-1 <= coluna.count(1)):
+    especificacoes=tabuleiro_especificacoes(tabuleiro)
+    E_lin=especificacoes[0]
+    E_col=especificacoes[1]
+    tab=tabuleiro[0]
+
+    for i in range(nr_lin_col):
+        coluna=[tab[l][i] for l in range(nr_lin_col)]
+        linha=tab[i]
+        if not (\
+                sum(E_lin[i]) == linha.count(2) and\
+                len(E_lin[i])-1 <= linha.count(1) and\
+                sum(E_col[i]) == coluna.count(2) and\
+                len(E_col[i])-1 <= coluna.count(1)\
+                ):
                     return False 
     return True 
 
@@ -246,26 +241,38 @@ def e_especificacao(t):
 # https://docs.python.org/2/library/stdtypes.html#string-formatting
 #escreve_tabuleiro(cria_tabuleiro((((2, ), (3, ), (2, ), (2, 2), (2, )), ((2, ), (1, 2), (2,3,4,5,6,7 ), (3, ), (3, )))))
 
-#################################################################################################################################
-#                                               PARA TESTAR                                                                     #
-#################################################################################################################################
 
 
+# testes do pdf com a resposta correcta logo em seguida
 e=(((2,), (3,), (2,), (2, 2), (2,)), ((2,), (1, 2), (2,), (3,), (3,)))
-t=cria_tabuleiro(e)
-print(tabuleiro_dimensoes(t))
+t = cria_tabuleiro(e)
+tabuleiro_dimensoes(t)
+print("(5, 5)")
 print(tabuleiro_especificacoes(t))
+print("(((2,), (3,), (2,), (2, 2), (2,)), ((2,), (1, 2), (2,), (3,), (3,)))")
 escreve_tabuleiro(t)
 t1 = tabuleiro_preenche_celula(t, cria_coordenada(4, 2), 2)
 escreve_tabuleiro(t)
 print(tabuleiros_iguais(t, t1))
+print("true")
 t2 = tabuleiro_preenche_celula(t, cria_coordenada(4, 3), 1)
 escreve_tabuleiro(t)
 print(tabuleiros_iguais(t, t1))
+print("true")
 print(tabuleiros_iguais(t, t2))
+print("true")
+print(tabuleiro_celula(t, cria_coordenada(4, 1)))
+print("0")
+print(tabuleiro_celula(t, cria_coordenada(4, 2)))
+print("2")
+print(tabuleiro_celula(t, cria_coordenada(4, 3)))
+print("1")
 print(e_tabuleiro(t))
+print("true")
 print(e_tabuleiro("a"))
+print("false")
 print(tabuleiro_completo(t))
+print("false")
 t2 = tabuleiro_preenche_celula(t, cria_coordenada(1, 1), 1)
 t2 = tabuleiro_preenche_celula(t, cria_coordenada(1, 2), 2)
 t2 = tabuleiro_preenche_celula(t, cria_coordenada(1, 3), 2)
@@ -291,3 +298,5 @@ t2 = tabuleiro_preenche_celula(t, cria_coordenada(5, 4), 1)
 t2 = tabuleiro_preenche_celula(t, cria_coordenada(5, 5), 1)
 escreve_tabuleiro(t)
 print(tabuleiro_completo(t))
+print("true")
+
